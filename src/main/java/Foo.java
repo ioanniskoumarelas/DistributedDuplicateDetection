3ba0e5e1b8544a34eef7e98b258a69bab8667b9e
John Koumarelas <john.koumarel@gmail.com>
2016-10-26 11:22:04 +0200

    Including a first dummy class w/ method.

---
+++ src/main/java/Foo.java
@@ -0,0 +1,12 @@
+/**
+ * Created by jokoum on 10/26/16.
+ */
+public class Foo {
+    public static void main(String[] args) {
+        System.out.println(getDummyString());
+    }
+
+    public static String getDummyString() {
+        return "Hello, World!";
+    }
+}
